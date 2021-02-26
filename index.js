5a50f92d53acc8015d6128c136134cf9a8f7fc15
alexmac <amatsibarov@gmail.com>
2021-02-26 13:03:35 +0300

    fix eslint warns

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 class VGenerator {
   constructor(charset, secretLen) {
-    this.currentIndex = 1;
-    this.charset = charset;
-    this.charsetLen = charset.length;
-    this.secretLen = secretLen;
-    this.totalVariants = this.charsetLen ** this.secretLen;
+    this.currentIndex = 1
+    this.charset = charset
+    this.charsetLen = charset.length
+    this.secretLen = secretLen
+    this.totalVariants = this.charsetLen ** this.secretLen
   }
 
   getNext() {
     if (this.currentIndex > this.totalVariants) {
-      return null;
+      return null
     }
 
-    let indexes = [];
+    let indexes = []
     for (let i = 0; i < this.secretLen; i++) {
       if (i === 0) {
-        indexes[i] = this.currentIndex % this.charsetLen;
+        indexes[i] = this.currentIndex % this.charsetLen
       } else {
-        indexes[i] = Math.ceil(this.currentIndex / this.charsetLen ** i);
+        indexes[i] = Math.ceil(this.currentIndex / this.charsetLen ** i)
       }
-      indexes[i] = indexes[i] % this.charsetLen;
+      indexes[i] = indexes[i] % this.charsetLen
       if (indexes[i] === 0) {
-        indexes[i] = this.charsetLen;
+        indexes[i] = this.charsetLen
       }
     }
-    this.currentIndex++;
+    this.currentIndex++
 
-    return indexes.map(i => this.charset[i - 1]).join('');
+    return indexes.map(i => this.charset[i - 1]).join('')
   }
 }
 
-module.exports = VGenerator;
+module.exports = VGenerator
--- test/index-spec.js
+++ test/index-spec.js
@@ -1,27 +1,27 @@
-const should = require('should');
-const VGenerator = require('../');
+const should = require('should')
+const VGenerator = require('../')
 
 describe('index / VGenerator', () => {
   describe('totalVariants', () => {
     it('should return total variants (case 1)', () => {
-      let vgen = new VGenerator('abc', 3);
-      should(vgen.totalVariants).equal(27);
-    });
+      let vgen = new VGenerator('abc', 3)
+      should(vgen.totalVariants).equal(27)
+    })
 
     it('should return total variants (case 2)', () => {
-      let vgen = new VGenerator('0123456789', 6);
-      should(vgen.totalVariants).equal(10 ** 6);
-    });
-  });
+      let vgen = new VGenerator('0123456789', 6)
+      should(vgen.totalVariants).equal(10 ** 6)
+    })
+  })
 
   describe('getNext', () => {
     it('should generate all variants for charset=xx and secret len=3', () => {
-      let vgen = new VGenerator('ab', 3);
-      let actual = [];
-      let next = vgen.getNext();
+      let vgen = new VGenerator('ab', 3)
+      let actual = []
+      let next = vgen.getNext()
       while (next) {
-        actual.push(next);
-        next = vgen.getNext();
+        actual.push(next)
+        next = vgen.getNext()
       }
 
       let expected = [
@@ -29,25 +29,25 @@ describe('index / VGenerator', () => {
         'aba', 'bba',
         'aab', 'bab',
         'abb', 'bbb'
-      ];
-      should(actual).eql(expected);
-    });
+      ]
+      should(actual).eql(expected)
+    })
 
     it('should generate all variants for charset=xxx and secret len=2', () => {
-      let vgen = new VGenerator('123', 2);
-      let actual = [];
-      let next = vgen.getNext();
+      let vgen = new VGenerator('123', 2)
+      let actual = []
+      let next = vgen.getNext()
       while (next) {
-        actual.push(next);
-        next = vgen.getNext();
+        actual.push(next)
+        next = vgen.getNext()
       }
 
       let expected = [
         '11', '21', '31',
         '12', '22', '32',
         '13', '23', '33'
-      ];
-      should(actual).eql(expected);
-    });
-  });
-});
+      ]
+      should(actual).eql(expected)
+    })
+  })
+})
